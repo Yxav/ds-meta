91a1bc10b693b4441041a130d612c7df32b577f9
Yuri Xavier <yuri_dsx_@hotmail.com>
2022-07-11 10:30:59 -0300

    datepicker with usestate

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import './styles.css'
+import './styles.css';
 
-import icon from '../../assets/img/notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg';
 
 function NotificationButton(){
     return (
@@ -10,4 +10,4 @@ function NotificationButton(){
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
