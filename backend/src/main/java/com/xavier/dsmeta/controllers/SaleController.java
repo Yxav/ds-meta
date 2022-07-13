9492120be4827554a4e756fb303dc3a79d366c00
Yuri Xavier <yuri_dsx_@hotmail.com>
2022-07-13 11:51:12 -0300

    Twilio SMS

--- backend/src/main/java/com/xavier/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/xavier/dsmeta/controllers/SaleController.java
@@ -2,13 +2,11 @@ package com.xavier.dsmeta.controllers;
 
 import com.xavier.dsmeta.entities.Sale;
 import com.xavier.dsmeta.services.SaleService;
+import com.xavier.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -17,12 +15,19 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping()
     public Page<Sale> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+    @GetMapping(value ="/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+        smsService.sendSms(id);
     }
 
 }
